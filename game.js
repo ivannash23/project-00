67f9b76c45ca8abf712404099c7cf4cd7039c2e7
ivannash23 <ivannash23@Gmail.com>
2017-02-02 10:10:16 -0800

    game version 1.0

--- game.js
+++ game.js
@@ -3,10 +3,12 @@ var countP2= 1;
 document.addEventListener("keydown", function(event) {
   	if(event.which == 39){
   		topCar();
+      polePosition();
   	}if(event.which == 65){
   		bottomCar();
+      polePosition();
   	}
-})
+}) 
 
 function topCar(){
 	$("#top-car").attr("class",`col-md-1 col-md-offset-${countP1}`);
@@ -21,7 +23,16 @@ function bottomCar(){
 	$("#bottom-car").attr("class",`col-md-1 col-md-offset-${countP2}`);
   		countP2++;
   		if(countP2 > 11){
-  			alert("You are not first, you are last! Player 2 Won!");
+  			alert("If you are not first, you are last! Player 2 Won!");
   			location.reload();
   		}
+}
+
+function polePosition(){
+  if(countP1 > countP2){
+    $("#pole").text("Pole Postion: Player 1");
+  }else{
+    $("#pole").text("Pole Position: Player 2");
+  }
+
 }
